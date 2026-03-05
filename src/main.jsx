2978036ceddccec09198d38e081ffc41d1d7abe5
mzeruesenay1 <mzeruesenay@outlook.com>
2026-03-05 12:57:08 -0500

    Replace App component with InterestForm component

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import InterestForm from './InterestForm'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <InterestForm />
   </React.StrictMode>
 )
